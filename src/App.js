c33c162be57bedba19e7d87e7d80bc935ba21e5e
Muhammad-Abdullah687 <114566052+Muhammad-Abdullah687@users.noreply.github.com>
2022-11-21 09:24:39 +0500

    Created a dummy app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Dummy React App</h1>
       </header>
+      <body>
+        <h1>Name:Muhammad Abdullah</h1>
+        <h1>Roll No: 20I-0687</h1>
+        <h1>Batch: 20</h1>
+        <h1>Section: R</h1>
+      </body>
     </div>
   );
 }
